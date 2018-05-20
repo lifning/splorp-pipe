04162d1df8d69ef32df1f75ed61a0f9554de807b
liffy <lifning@protonmail.com>
2018-05-19 23:11:47 -0700

    put presumed packet meanings we observed today

--- negotiate.py
+++ negotiate.py
@@ -8,6 +8,13 @@ SPLOON_PORT_RECV = 49152
 SPLOON_PORT_SEND = 49153
 WOB_SOCK = 'ws://hello.buttslol.net:8765'
 
+MAGIC = b'2\xab\x98d'
+
+PACKET_HOST_SEARCH = b'\2\0\0\0'
+PACKET_JOIN_SEARCH = b'\1\0\0\0'
+
+# binds to empty-string are for listening to broadcast packets
+
 send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 send_sock.bind(('', SPLOON_PORT_SEND))
@@ -27,5 +34,9 @@ logging.getLogger().setLevel('DEBUG')
 while True:
     data, addr = recv_sock.recvfrom(1024)
     telepathy.outboundBlobs.put_nowait(data)
-    if not data.startswith(b'2\xab\x98d\x02\x00\x00\x00'):
+    if not data.startswith(MAGIC):
         logging.warning(f'\nweird data from {addr}: {data}')
+    elif data[4:8] == PACKET_HOST_SEARCH:
+        logging.debug(f'\nhost search {data[8:]}')
+    elif data[4:8] == PACKET_JOIN_SEARCH:
+        logging.debug(f'\njoin search {data[8:]}')
